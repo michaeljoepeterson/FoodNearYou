edb0a25e61565eaa18551bad4b04fbab514049cc
Michael Peterson <michaeljoepeterson@gmail.com>
2018-04-27 08:12:26 -0600

    added labels to markers and section for results

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 function initMap(latitude,longitude) {
   let uluru = {lat: latitude, lng: longitude};
   let map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 20,
+    zoom: 15,
     center: uluru
   });
   let marker = new google.maps.Marker({
     position: uluru,
-    map: map
+    map: map,
+    label: "1"
   });
   return map
 }
@@ -14,14 +15,15 @@ function addMarker(latitude,longitude,map){
   let uluru = {lat: latitude, lng: longitude};
   let marker = new google.maps.Marker({
     position: uluru,
-    map: map
+    map: map,
+    label: "2"
   });  
 }
 //$(initMap);
 function submitClicked(){
   $(".submitForm").submit(function(event){
     event.preventDefault();
-    map1 = initMap(53.5182744,-113.4953357);
+    let map1 = initMap(53.5182744,-113.4953357);
     addMarker(53.5176707,-113.4995439,map1)
     addMarker(53.552364,-113.4961727,map1)
     addMarker(53.518218,-113.5007353,map1)

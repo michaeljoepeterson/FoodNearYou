e17564e1591402d4f734f648d53c6d4f152ca216
Michael Peterson <michaeljoepeterson@gmail.com>
2018-04-27 07:57:58 -0600

    added ability to add multiple markers

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
 function initMap(latitude,longitude) {
-  var uluru = {lat: latitude, lng: longitude};
-  var map = new google.maps.Map(document.getElementById('map'), {
+  let uluru = {lat: latitude, lng: longitude};
+  let map = new google.maps.Map(document.getElementById('map'), {
     zoom: 20,
     center: uluru
   });
-  var marker = new google.maps.Marker({
+  let marker = new google.maps.Marker({
     position: uluru,
     map: map
   });
+  return map
+}
+function addMarker(latitude,longitude,map){
+  let uluru = {lat: latitude, lng: longitude};
+  let marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });  
 }
 //$(initMap);
 function submitClicked(){
   $(".submitForm").submit(function(event){
     event.preventDefault();
-    initMap(33.8120918,-117.9211682);
+    map1 = initMap(53.5182744,-113.4953357);
+    addMarker(53.5176707,-113.4995439,map1)
+    addMarker(53.552364,-113.4961727,map1)
+    addMarker(53.518218,-113.5007353,map1)
     console.log($(".jsResults").val());
   });
 }
